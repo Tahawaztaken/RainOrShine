9e8b4c1c85fed2086d397b2d742ece5930b4942a
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-09-18 19:05:12 -0400

    testing filepath

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// import * as THREE from 'three'
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
+// import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
+// import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene()
 
